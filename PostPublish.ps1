998981d54e9f766f427eefb8da334848ac741f54
RaidMax <raidmax@live.com>
2019-12-09 14:01:02 -0600

    update the json output encoding

--- PostPublish.ps1
+++ PostPublish.ps1
@@ -20,4 +20,4 @@ Minor = $versionInfo.ProductMinorPart
 Build = $versionInfo.ProductBuildPart  
 Revision = $versionInfo.ProductPrivatePart
 }
-$json | ConvertTo-Json > ("{0}\VersionInformation.json" -f $PublishDir)
+$json | ConvertTo-Json -Encoding ASCII > ("{0}\VersionInformation.json" -f $PublishDir)

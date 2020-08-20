45f1215aae3ddaa9c24d84931beef2c832874a13
RaidMax <raidmax@live.com>
2020-08-20 11:08:21 -0500

    include "all" meta button on profile include full humanizer package to library bug in russian translations

--- WebfrontCore/Startup.cs
+++ WebfrontCore/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SharedLibraryCore;

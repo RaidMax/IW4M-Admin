3d5d2f9a7a06e018a258bc31c4fa98e389737e57
Edo <edoardo.sanguineti222@gmail.com>
2021-11-15 17:54:14 +0000

    Parser IW4X Add missing semicolon
    Add missing semicolon

--- Plugins/ScriptPlugins/ParserIW4x.js
+++ Plugins/ScriptPlugins/ParserIW4x.js
@@ -18,7 +18,7 @@ var plugin = {
         rconParser.Configuration.CommandPrefixes.Say     = 'sayraw {0}';
         rconParser.Configuration.CommandPrefixes.Kick    = 'clientkick {0} "{1}"';
         rconParser.Configuration.CommandPrefixes.Ban     = 'clientkick {0} "{1}"';
-        rconParser.Configuration.CommandPrefixes.TempBan = 'tempbanclient {0} "{1}"'
+        rconParser.Configuration.CommandPrefixes.TempBan = 'tempbanclient {0} "{1}"';
         
         rconParser.Configuration.DefaultRConPort                = 28960;
         rconParser.Configuration.DefaultInstallationDirectoryHint   = 'HKEY_CURRENT_USER\\Software\\Classes\\iw4x\\shell\\open\\command';

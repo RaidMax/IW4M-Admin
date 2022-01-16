23ed6ac0cb6f276615a27265eb1e5af14bc785ff
RaidMax <raidmax@live.com>
2022-01-16 10:53:31 -0600

    Add Plutonium T4 Co-Op/Zombies support

--- Plugins/ScriptPlugins/ParserPlutoniumT4COZM.js
+++ Plugins/ScriptPlugins/ParserPlutoniumT4COZM.js
@@ -2,9 +2,9 @@
 var eventParser;
 
 var plugin = {
-    author: 'RaidMax, Chase',
-    version: 0.3,
-    name: 'Plutonium T4 MP Parser',
+    author: 'RaidMax',
+    version: 0.1,
+    name: 'Plutonium T4 CO-OP/Zombies Parser',
     isParser: true,
 
     onEventAsync: function (gameEvent, server) {
@@ -13,23 +13,26 @@ var plugin = {
     onLoadAsync: function (manager) {
         rconParser = manager.GenerateDynamicRConParser(this.name);
         eventParser = manager.GenerateDynamicEventParser(this.name);
-        
+
         rconParser.Configuration.CommandPrefixes.Kick         = 'clientkick {0}';
         rconParser.Configuration.CommandPrefixes.Ban          = 'clientkick {0}';
         rconParser.Configuration.CommandPrefixes.TempBan      = 'clientkick {0}';
         rconParser.Configuration.CommandPrefixes.RConResponse = '\xff\xff\xff\xffprint\n';
         rconParser.Configuration.GuidNumberStyle              = 7; // Integer
         rconParser.Configuration.DefaultRConPort              = 28960;
-        
+        rconParser.Configuration.CommandPrefixes.RConGetInfo  = undefined;
+
+
         rconParser.Configuration.DefaultInstallationDirectoryHint = '{LocalAppData}/Plutonium/storage/t4';
-        
-        rconParser.Version  = 'Plutonium T4';
+        rconParser.Configuration.OverrideDvarNameMapping.Add('fs_homepath', 'fs_localAppData');
+
+        rconParser.Version  = 'Plutonium T4 Singleplayer';
         rconParser.GameName = 5; // T4
 
         eventParser.Configuration.GuidNumberStyle = 7; // Integer
-        eventParser.Configuration.GameDirectory   = 'raw';
- 
-        eventParser.Version  = 'Plutonium T4';
+        eventParser.Configuration.GameDirectory   = 'main';
+
+        eventParser.Version  = 'Plutonium T4 Singleplayer';
     },
 
     onUnloadAsync: function () {

97309f8d4d98613b1f03481058dbf99e653a4a59
RaidMax <raidmax@live.com>
2022-01-20 11:41:26 -0600

    Update shared library to reference data library instead of separate nuget package

--- SharedLibraryCore/TagHelpers/ColorCode.cs
+++ SharedLibraryCore/TagHelpers/ColorCode.cs
@@ -1,28 +1,35 @@
 ﻿using System.Linq;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using SharedLibraryCore.Configuration;
 
 namespace SharedLibraryCore
 {
     [HtmlTargetElement("color-code")]
     public class ColorCode : TagHelper
     {
+        public ColorCode(ApplicationConfiguration appConfig)
+        {
+            _allow = appConfig?.EnableColorCodes ?? false;
+        }
+
         public string Value { get; set; }
 
-        public bool Allow { get; set; } = false;
+        private readonly bool _allow;
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "ColorCode";
             output.TagMode = TagMode.StartTagAndEndTag;
 
-            if (Allow)
+            if (_allow)
             {
                 var matches = Regex.Matches(Value, @"\^([0-9]|\:)([^\^]*)");
                 foreach (Match match in matches)
                 {
-                    char colorCode = match.Groups[1].ToString().Last();
-                    output.PreContent.AppendHtml($"<span class='text-color-code-{(colorCode >= 48 && colorCode <= 57 ? colorCode.ToString() : ((int)colorCode).ToString())}'>");
+                    var colorCode = match.Groups[1].ToString().Last();
+                    output.PreContent.AppendHtml(
+                        $"<span class='text-color-code-{(colorCode >= 48 && colorCode <= 57 ? colorCode.ToString() : ((int) colorCode).ToString())}'>");
                     output.PreContent.Append(match.Groups[2].ToString());
                     output.PreContent.AppendHtml("</span>");
                 }

c6042632dd3b8d54512cd25608e8de70349c124a
RaidMax <raidmax@live.com>
2022-06-02 18:30:22 -0500

    add default anticheat detection types

--- Plugins/Stats/Config/AnticheatConfiguration.cs
+++ Plugins/Stats/Config/AnticheatConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using static IW4MAdmin.Plugins.Stats.Cheat.Detection;
 using static SharedLibraryCore.Server;
 
@@ -10,7 +11,13 @@ namespace Stats.Config
         public bool Enable { get; set; }
         [Obsolete]
         public IDictionary<long, DetectionType[]> ServerDetectionTypes { get; set; } = new Dictionary<long, DetectionType[]>();
-        public IDictionary<Game, DetectionType[]> GameDetectionTypes { get; set; } = new Dictionary<Game, DetectionType[]>();
+
+        public IDictionary<Game, DetectionType[]> GameDetectionTypes { get; set; } =
+            new Dictionary<Game, DetectionType[]>()
+            {
+                { Game.IW4, Enum.GetValues(typeof(DetectionType)).Cast<DetectionType>().ToArray() },
+                { Game.T6, new[] { DetectionType.Offset, DetectionType.Snap, DetectionType.Strain } }
+            };
         public IList<long> IgnoredClientIds { get; set; } = new List<long>();
         public IDictionary<Game, IDictionary<DetectionType, string[]>> IgnoredDetectionSpecification{ get; set; } = new Dictionary<Game, IDictionary<DetectionType, string[]>>
         {

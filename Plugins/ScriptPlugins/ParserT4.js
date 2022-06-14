32c4ac5bf32e8415b3a99a66de94d1f010c4b05d
RaidMax <raidmax@live.com>
2022-06-14 15:12:19 -0500

    set the rcon parser game name for retail WaW

--- Plugins/ScriptPlugins/ParserT4.js
+++ Plugins/ScriptPlugins/ParserT4.js
@@ -3,7 +3,7 @@ var eventParser;
 
 var plugin = {
     author: 'RaidMax',
-    version: 0.2,
+    version: 0.3,
     name: 'Call of Duty 5: World at War Parser',
     isParser: true,
 
@@ -17,6 +17,7 @@ var plugin = {
         rconParser.Configuration.GuidNumberStyle = 7; // Integer
         rconParser.Configuration.DefaultRConPort = 28960;
         rconParser.Version = 'Call of Duty Multiplayer COD_WaW MP build 1.7.1263 CL(350073) JADAMS2 Thu Oct 29 15:43:55 2009 win-x86';
+        rconParser.GameName = 5; // T4
         
         eventParser.Configuration.GuidNumberStyle = 7; // Integer
         eventParser.GameName = 5; // T4

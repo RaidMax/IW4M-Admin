7f09ce90aca26d0405d9c5025715d77a5a4fc822
RaidMax <raidmax@live.com>
2023-05-27 12:15:22 -0500

    possible fix for remotely loaded plugins

--- Application/Misc/RemoteAssemblyHandler.cs
+++ Application/Misc/RemoteAssemblyHandler.cs
@@ -13,10 +13,10 @@ namespace IW4MAdmin.Application.Misc
 {
     public class RemoteAssemblyHandler : IRemoteAssemblyHandler
     {
-        private const int keyLength = 32;
-        private const int tagLength = 16;
-        private const int nonceLength = 12;
-        private const int iterationCount = 10000;
+        private const int KeyLength = 32;
+        private const int TagLength = 16;
+        private const int NonceLength = 12;
+        private const int IterationCount = 10000;
 
         private readonly ApplicationConfiguration _appconfig;
         private readonly ILogger _logger;
@@ -30,7 +30,7 @@ namespace IW4MAdmin.Application.Misc
         public IEnumerable<Assembly> DecryptAssemblies(string[] encryptedAssemblies)
         {
             return DecryptContent(encryptedAssemblies)
-                .Select(decryptedAssembly => Assembly.Load(decryptedAssembly));
+                .Select(Assembly.Load);
         }
 
         public IEnumerable<string> DecryptScripts(string[] encryptedScripts)
@@ -38,24 +38,24 @@ namespace IW4MAdmin.Application.Misc
             return DecryptContent(encryptedScripts).Select(decryptedScript => Encoding.UTF8.GetString(decryptedScript));
         }
 
-        private byte[][] DecryptContent(string[] content)
+        private IEnumerable<byte[]> DecryptContent(string[] content)
         {
             if (string.IsNullOrEmpty(_appconfig.Id) || string.IsNullOrWhiteSpace(_appconfig.SubscriptionId))
             {
                 _logger.LogWarning($"{nameof(_appconfig.Id)} and {nameof(_appconfig.SubscriptionId)} must be provided to attempt loading remote assemblies/scripts");
-                return new byte[0][];
+                return Array.Empty<byte[]>();
             }
 
             var assemblies = content.Select(piece =>
             {
-                byte[] byteContent = Convert.FromBase64String(piece);
-                byte[] encryptedContent = byteContent.Take(byteContent.Length - (tagLength + nonceLength)).ToArray();
-                byte[] tag = byteContent.Skip(byteContent.Length - (tagLength + nonceLength)).Take(tagLength).ToArray();
-                byte[] nonce = byteContent.Skip(byteContent.Length - nonceLength).Take(nonceLength).ToArray();
-                byte[] decryptedContent = new byte[encryptedContent.Length];
+                var byteContent = Convert.FromBase64String(piece);
+                var encryptedContent = byteContent.Take(byteContent.Length - (TagLength + NonceLength)).ToArray();
+                var tag = byteContent.Skip(byteContent.Length - (TagLength + NonceLength)).Take(TagLength).ToArray();
+                var nonce = byteContent.Skip(byteContent.Length - NonceLength).Take(NonceLength).ToArray();
+                var decryptedContent = new byte[encryptedContent.Length];
 
-                var keyGen = new Rfc2898DeriveBytes(Encoding.UTF8.GetBytes(_appconfig.SubscriptionId), Encoding.UTF8.GetBytes(_appconfig.Id.ToString()), iterationCount, HashAlgorithmName.SHA512);
-                var encryption = new AesGcm(keyGen.GetBytes(keyLength));
+                var keyGen = new Rfc2898DeriveBytes(Encoding.UTF8.GetBytes(_appconfig.SubscriptionId), Encoding.UTF8.GetBytes(_appconfig.Id), IterationCount, HashAlgorithmName.SHA512);
+                var encryption = new AesGcm(keyGen.GetBytes(KeyLength));
 
                 try
                 {

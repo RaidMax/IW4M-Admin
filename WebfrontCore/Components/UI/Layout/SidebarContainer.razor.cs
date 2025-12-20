7a371568389c44aaceb6fb87d2d1049ae7837780
RaidMax <raidmax@live.com>
2025-12-20 12:38:36 -0600

    Move search for for mobile

---
+++ WebfrontCore/Components/UI/Layout/SidebarContainer.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace WebfrontCore.Components.UI.Layout;
+
+public partial class SidebarContainer
+{
+    protected override void OnInitialized()
+    {
+        AppState.OnChange += OnAppStateChange;
+    }
+
+    private void OnAppStateChange()
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        AppState.OnChange -= OnAppStateChange;
+    }
+
+    private void ToggleMobile()
+    {
+        AppState.IsMobileNavOpen = false;
+    }
+}

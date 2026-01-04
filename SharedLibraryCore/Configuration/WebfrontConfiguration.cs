d9d728283f82b67df99111a7ec62e39a3407a6f4
RaidMax <raidmax@live.com>
2026-01-03 22:33:24 -0600

    Migration webfront permissions configuration

--- SharedLibraryCore/Configuration/WebfrontConfiguration.cs
+++ SharedLibraryCore/Configuration/WebfrontConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace SharedLibraryCore.Configuration;
+﻿using static Data.Models.Client.EFClient;
+
+namespace SharedLibraryCore.Configuration;
 
 public class WebfrontConfiguration
 {
@@ -12,4 +14,74 @@ public class WebfrontConfiguration
     public string SecondaryColor { get; set; } = "pink";
     public string ThemePreset { get; set; } = "minimal";
     public bool PreventUserCustomization { get; set; }
+    
+    public Dictionary<string, List<string>> PermissionSets { get; set; } = new()
+    {
+        {
+            nameof(Permission.User), [
+                "HelpPage.Read",
+                "ProfilePage.Read",
+                "Interaction.Read"
+            ]
+        },
+        {
+            nameof(Permission.Trusted), [
+                "HelpPage.Read",
+                "ProfilePage.Read",
+                "Penalty.Read",
+                "Interaction.Read",
+                "ClientLevel.Read",
+                "ConsolePage.Read",
+                "PrivilegedClientsPage.Read"
+            ]
+        },
+        {
+            nameof(Permission.Moderator), [
+                "HelpPage.Read",
+                "ProfilePage.Read",
+                "Penalty.Read",
+                "Interaction.Read",
+                "ClientLevel.Read",
+                "PrivilegedClientsPage.Read",
+                "AdminMenu.Read",
+                "RecentPlayersPage.Read",
+                "ClientNote.Read",
+                "ConsolePage.Read",
+                "AdvancedSearch.Read"
+            ]
+        },
+        {
+            nameof(Permission.Administrator), [
+                "HelpPage.Read",
+                "ProfilePage.Read",
+                "Penalty.Read",
+                "Interaction.Read",
+                "ClientLevel.Read",
+                "PrivilegedClientsPage.Read",
+                "AdminMenu.Read",
+                "RecentPlayersPage.Read",
+                "ClientNote.Read",
+                "AdvancedSearch.Read",
+                "MetaAliasUpdate.Read",
+                "ClientGuid.Read",
+                "ConsolePage.Read",
+                "AuditPage.Read"
+            ]
+        },
+        {
+            nameof(Permission.SeniorAdmin), [
+                "*"
+            ]
+        },
+        {
+            nameof(Permission.Owner), [
+                "*"
+            ]
+        },
+        {
+            nameof(Permission.Console), [
+                "*"
+            ]
+        }
+    };
 }
